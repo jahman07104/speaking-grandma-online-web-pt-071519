5f2275faffb28a1f58571a492f365a3f07c17908
Patrick Harrison <jahman@bellatlantic.net>
2019-07-16 03:15:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(phrase)
    if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
+<<<<<<< HEAD
    elsif phrase =='Hi Nana, how are you?'
      "HUH?! SPEAK UP, SONNY!" 
    elsif phrase == "Hi!"
@@ -13,4 +14,24 @@ def speak_to_grandma(phrase)
 
 end
 end
+=======
+   elsif phrase == "i love you grandma!"
+   "HUH?! SPEAK UP, SONNY!"
+   if phrase =='Hi Nana, how are you?'
+     "HUH?! SPEAK UP, SONNY!" 
+#   if phrase == "Hi"
+# "HUH?! SPEAK UP, SONNY!" 
+# if phrase == "WHAT DID YOU EAT TODAY"
+#   "NO, NOT SINCE 1938!"
+#   elsif
+#   "NO, NOT SINCE 1938!"
+# else "WHAT"
+
+end
+end
+end
+# Whatever you say to grandma, she should respond with
+# HUH?! SPEAK UP, SONNY!
+# unless you shout it (type in all capitals).
+>>>>>>> 6675817039d5f6e3fc28390d5ff8f86b07658ff5
 
